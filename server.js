1c4647759789d39476c115afcd1f8fc8b72d9ab2
Bholanq <akashhem360@gmail.com>
2025-07-08 23:29:00 +0530

    fifth commit

--- server.js
+++ server.js
@@ -18,14 +18,7 @@ const allowedOrigins = [
   'https://voting-app-frontend.vercel.app',
 ];
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: '*'
 }));
 
 app.use(express.json()); // Body parser middleware 
